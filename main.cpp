273c29faf44a988a34555d63c9f9559ae2543131
S5265415 <101595525+S5265415@users.noreply.github.com>
2022-04-06 09:43:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+    int n;
+    int a=1,b=0,c;
+    std::cin>>n;
+    std::cout<<"1"<< std::endl;
+    while(n>=(a+b)){
+        c= a+b;
+        std::cout<<c<<std::endl;
+        b=a;
+        a=c;
+    }
    return 0;
 }
